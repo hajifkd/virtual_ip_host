9bade5847635663049781975aa8a58a56826b644
Hajime Fukuda <hajime.fukuda@me.com>
2019-05-20 23:25:07 +0900

    added a tiny example to read a packet

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,6 @@
 extern crate libc;
 
-use libc::{__errno_location, c_int, socket, strerror, ETH_P_ALL, PF_PACKET, SOCK_RAW};
-use std::ffi::CStr;
+use libc::{c_int, socket, recv, ETH_P_ALL, PF_PACKET, SOCK_RAW, PT_NULL};
 
 mod utils;
 
@@ -11,7 +10,14 @@ fn main() {
         println!("fd: {}", s);
         if s == -1 {
             utils::show_error_text();
+            return;
         }
+
+        let length = 128;
+        let mut buf = vec![0u8; length];
+        let r = recv(s, buf.as_mut_ptr() as _, length, 0);
+        dbg!(r);
+        dbg!(buf);
     }
     println!("Hello, world!");
 }
