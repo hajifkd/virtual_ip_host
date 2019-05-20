5663044d0f94986f7e123b4af3bdbab2022b473d
Hajime Fukuda <hajime.fukuda@me.com>
2019-05-21 02:19:07 +0900

    fixed a bug

--- src/utils.rs
+++ src/utils.rs
@@ -8,11 +8,22 @@ pub fn swap_endian_16(t: u16) -> u16 {
 
 // little <-> big for 16bit
 pub fn swap_endian_32(t: u32) -> u32 {
-    t << 24 | (t << 16 & 0x00_FF_00_00) | (t >> 16 & 0x00_00_FF_00) | t >> 24
+    t << 24 | (t << 8 & 0x00_FF_00_00) | (t >> 8 & 0x00_00_FF_00) | t >> 24
 }
 
 pub unsafe fn show_error_text() {
     let errno = *__errno_location();
     let error_str = CStr::from_ptr(strerror(errno));
     println!("{}", error_str.to_str().unwrap());
+}
+
+#[cfg(test)]
+mod test {
+    use crate::utils::*;
+
+    #[test]
+    fn test_swap_endian() {
+        assert_eq!(0xFF_00u16, swap_endian_16(0x00_FFu16));
+        assert_eq!(0xFF_00_AA_88u32, swap_endian_32(0x88_AA_00_FFu32));
+    }
 }
